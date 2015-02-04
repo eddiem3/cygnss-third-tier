2308369b45b89a00c0773a1c2f87e6b91085e30d
Eddie Massey <eem33@case.edu>
2015-02-03 22:11:04 -0500

    match filter inputs float[]

--- cross_correlation.cpp
+++ cross_correlation.cpp
@@ -9,25 +9,31 @@
 
 using namespace af;
 
+//extern {
+// correlateSignals_()
+//}
+
 
 /* A function to compute the cross correlation of two signals.
    Useful for finding the correlation of a waveform and a storm signal
    @param signal - The waveform database array
    @param storm - The real storm array 
 */
-static array correlateSignals(array signal, array storm)
+static array correlateSignals(float signalArray[], float stormArray[])
 {
 
-  array fourierSignal;
-  array fourierStorm;
 
+  //Convert the storm signals into Array Fire array type
+  array fourierSignal(600,1,signalArray);
+  array fourierStorm(600,1,stormArray);
+ 
   //compute the fft of the signal in the waveform database
-  fourierSignal = fft(signal);  
+  fourierSignal = fft(fourierSignal);  
 
   //print("fdmims", fourierSignal.dims());
 
   //compute the complex conjugate of the fft of the storm 
-  fourierStorm = conjg(fft(storm));
+  fourierStorm = conjg(fft(fourierStorm));
   
 
   //make sure that the variables associated with each operation are evaulated
@@ -37,7 +43,6 @@ static array correlateSignals(array signal, array storm)
   
   //magnitude 
   //sum and multiply signals
-
   array numerator = conjg(sum(fourierSignal * fourierStorm)) * sum(fourierSignal * fourierStorm);
 
   array denominator = sum(conjg(fourierSignal) * fourierSignal) * sum(conjg(fourierStorm) * fourierStorm);
@@ -55,24 +60,26 @@ static array correlateSignals(array signal, array storm)
 int main(int argc, char ** argv)
 {
   try {
-
+    
     //Select a device and display info
     int device = argc > 1 ? atoi(argv[1]) : 0;
     deviceset(device);
     info();
+    
+    float signalA[600];
+    float signalB[600];
 
+    for(int i = 0; i < 600; i++)
+      {
+	signalA[i] = i;
+	signalB[i] = i;
+      }
 
-    //    timer start = timer::start();
-
-    //for(int i=1; i < 600; i++)
-    // {
-    array A = randu(600,1, f32);
-    array B = randu(600,1, f32);
-    correlateSignals(A,A);
-	//}
-	//std::cout << timer::stop(start) << std::endl;
-            
-  } catch (af::exception& e) {
+    // A = randu(600,1, f32);
+    //array B = randu(600,1, f32);
+    correlateSignals(signalA,signalB);
+    
+  }             catch (af::exception& e) {
     fprintf(stderr, "%s\n", e.what());
   }
   return 0;
