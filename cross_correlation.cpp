7285a5080b53f21d7c86e3c2003fc9f01407dc11
Eddie Massey <eem33@case.edu>
2015-02-04 22:40:31 -0500

    reading data from interpolated file

--- cross_correlation.cpp
+++ cross_correlation.cpp
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
+#include <fstream>
+#include <iterator>
+#include <stdio.h>
 
 //Array Fire Includes
 #include <arrayfire.h>
 #include <af/util.h>
 
 using namespace af;
-
-//extern {
-// correlateSignals_()
-//}
 
 
 /* A function to compute the cross correlation of two signals.
@@ -59,28 +58,155 @@ static array correlateSignals(float signalArray[], float stormArray[])
 
 int main(int argc, char ** argv)
 {
-  try {
-    
-    //Select a device and display info
-    int device = argc > 1 ? atoi(argv[1]) : 0;
-    deviceset(device);
-    info();
-    
-    float signalA[600];
-    float signalB[600];
+  //Read in Nature Run Data
+  std::cout << "Reading interpolated nature run data" << "\n";
+  std::ifstream nature("interpolatedNatureRun.txt");
 
-    for(int i = 0; i < 600; i++)
-      {
-	signalA[i] = i;
-	signalB[i] = i;
-      }
+  std::vector<array> nature_vectors;
 
-    // A = randu(600,1, f32);
-    //array B = randu(600,1, f32);
-    correlateSignals(signalA,signalB);
+  long double n;
+  while(!nature.eof())
+    {
+      array nature_signal(600,1);
+	
+      for(int i = 0; i < nature_signal.dims(0); i++)
+	{
+	  nature >> n;
+	  nature_signal(i) = n;	  
+	  //std::cout << n << "\n";
+	}
+      nature_vectors.push_back(nature_signal);
+    }
+
+  std::cout << nature_vectors.size() << "\n";
+
+  print("a",nature_vectors.at(1));
+
+
+  //  for(int i=0; i < nature_vectors.size(); i++){
+  //print("a",nature_vectors[i]);
+  //  }
+
+
+
+  // for(std::vector<int>::const_iterator it = nature_vectors.begin(); 
+  //     it != nature_vectors.end(); ++it)
+  //   {
+  //     print("a",*it);
+  //   }
+
+
+
+  
+
+  //Convert std::vectors into arrays
+  //Note: The willoughby data is VERY LARGE
+  //Allocate on the heap instead of the stack or you
+  //will get a segmentation fault
+  //std::cout << "Converting will to an array" << "\n";
+  //float *willoughby = (float *)malloc(33996600 * sizeof(float));
+  //std::copy(will.begin(), will.end(), willoughby);
+  
+  //std::cout << "Converting nature to an array" << "\n";
+  //float natureRun[12444];
+  //std::copy(bob.begin(), bob.end(),natureRun);
+
+  
+  
+  
+
+
+  
+ 
+
+
+
+
+  // std::cout << "Splitting big willougby array into subarrays" << "\n";
+  // for(int i =0; i < 33996600; i++)
+  //   {
+  //     float data[600];
+
+  //     for(int j = 0; j < 600; j++)
+  // 	{
+  // 	  data[j] = willoughby[i + 600 * j];
+  // 	}	   
+  //     array data_array(600,1,data);
+  //     willougbyModels.push_back(data_array);      
+  //   }
+
+  // std::cout << "splitting up nature array" << "\n";
+  // for(int i=0; i < 12444; i++)
+  //   {
+  //     float data[600];
+
+  //     for(int j=0; j < 600; j++)
+  // 	{
+  // 	  data[j] = natureRun[i + 600 * j];
+  // 	}
+
+  //     array data_array(600,1,data);
+  //     natureRunData.push_back(data_array);
+
+  //   }
+ 
+
+  
+  // for (int i=0; i<natureRunData.size(); i++) 
+  //   {
+  //     print("a",natureRunData[i]);
+  //   } 
+
+	  
+  //Loop through each array in the storm vector 
+  //Multiply it with each of the willougby
+  //  for(auto &i : natureRunData)
+  //{
+  //  i * i;
+  //}
+  
+
+
+
+
+
+
+  //DON'T DELETE
+  //print to std out
+  // std::cout << "numbers read in:\n";
+  // std::copy(bob.begin(), bob.end(), 
+  //           std::ostream_iterator<float>(std::cout, " "));
+  // std::cout << std::endl;
+
+
+  //
+
+
+
+  // try {
     
-  }             catch (af::exception& e) {
-    fprintf(stderr, "%s\n", e.what());
-  }
+  //   //Select a device and display info
+  //   int device = argc > 1 ? atoi(argv[1]) : 0;
+  //   deviceset(device);
+  //   info();
+    
+  //   float signalA[600];
+  //   float signalB[600];
+
+  //   for(int i = 0; i < 600; i++)
+  //     {
+  // 	signalA[i] = i;
+  // 	signalB[i] = i;
+  //     }
+
+  //   // A = randu(600,1, f32);
+  //   //array B = randu(600,1, f32);
+  //   correlateSignals(signalA,signalB);
+    
+  // }             catch (af::exception& e) {
+  //   fprintf(stderr, "%s\n", e.what());
+  // }
+
+  //  free(willoughby);
   return 0;
 }
